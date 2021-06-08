2c7ec3d056617c76b41df239d4ba70744a6f15f6
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-08 13:31:36 +0200

    added an event to welcome users when clicking on the login button

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 (function() {
     console.warn('Script correctly linked to the web page');
 
+    document.forms[0].elements[3].addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let nameValue = document.getElementById('name').value;
+        let passValue = document.getElementById('pass').value;
+        let message = `Welcome ${nameValue}`;
+        alert(message);
+    });
 })();
