8f34b55f6fe1fb14d64a5df4231b9d9760decb14
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-08 16:51:56 +0200

    put the field selectors outside event handlers - on pressing login the pw field is clear too

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 (function() {
     console.warn('Script correctly linked to the web page');
+    
+    let nameField = document.getElementById('name');
+    let passField = document.getElementById('pass');
+    
 
     document.forms[0].elements.login_btn.addEventListener('click', function(e) {
         e.preventDefault();
 
-        let nameField = document.getElementById('name');    
         
         let message = `Welcome ${nameField.value}`;
         alert(message);
         nameField.value = '';
+        passField.value = '';
     });
 })();
