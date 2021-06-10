1dc0b3547fd2dc8d58ceaa625c53163d68a796c7
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-10 11:16:02 +0200

    now clicking on show password shows letters in the password field as text

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
     
     let nameField = document.getElementById('name');
     let passField = document.getElementById('pass');
+    let showPwChk = document.getElementById('showText');
     
-
     document.forms[0].elements.login_btn.addEventListener('click', function(e) {
         e.preventDefault();
 
@@ -13,5 +13,9 @@
         alert(message);
         nameField.value = '';
         passField.value = '';
+    });
+
+    showPwChk.addEventListener('change', function() {
+        passField.type = 'text';
     });
 })();
